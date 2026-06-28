bde60ae01373d74300ccc01dce4ceaff70ac2772
Gen <genli@cs.stanford.edu>
2026-06-28 23:49:42 +0200

    Update show_bib.js

--- show_bib.js
+++ show_bib.js
@@ -1,7 +1,6 @@
 function showBib(btn, bib){
     const box = document.getElementById(bib);
     box.hidden = !box.hidden;
-    // 只改最后那个文字节点，保留 <i> 图标
     const label = btn.lastChild;
-    label.textContent = box.hidden ? " Show BibTex" : " Hide BibTex";
+    label.textContent = box.hidden ? " BibTex" : " Hide BibTex";
 }
